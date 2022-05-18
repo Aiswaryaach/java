66af6cb907624c20735e6eb906833104d9a295bc
Aiswaryaach <aiswaryasudhakaran224@gmail.com>
2022-05-18 13:44:33 +0530

    six commit

---
+++ org/shape/Circle.java
@@ -0,0 +1,13 @@
+package.org.shape;
+public class Circle
+{
+private int rad;
+public Circle(int r)
+{
+rad=r;
+}
+public double area()
+{
+return (3.14*rad*rad);
+}
+}
---
+++ org/shape/Square.java
@@ -0,0 +1,15 @@
+package.org.shape;
+public class Square
+{
+int side;
+public Square(int s)
+{
+side=s;
+}
+
+public int area()
+{
+return (side*side);
+}
+}
+

939d5ac0329f53e6e0ff023ba2c51013debecc03
Aiswaryaach <aiswaryasudhakaran224@gmail.com>
2022-06-07 15:28:09 +0530

    five commit

--- awt/Line.java
+++ awt/Line.java
@@ -7,13 +7,14 @@ public class Line extends Applet
  g.drawLine(100,10,50,50);
  g.drawRect(300,50,70,50);
  g.drawOval(200,20,70,70);
-g.drawOval(80, 70, 150, 150);
+g.setColor(Color.yellow);
+g.fillOval(80, 70, 150, 150);
  
-       
+  g.setColor(Color.black);    
  g.fillOval(120, 120, 15, 15);
  g.fillOval(170, 120, 15, 15);
  
-   g.drawArc(130, 180, 50, 20, 180, 180);
+   g.drawArc(115, 140, 80, 50, 180, 190);
     }
 
 
